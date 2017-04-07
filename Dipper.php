ba8936984e5d986154f7800f5069d1ba22a4f0b3
Knut Kohl <github@knutkohl.de>
2017-04-07 14:15:02 +0200

    Add parseFile method to parse YAML files drectly

--- Dipper.php
+++ Dipper.php
@@ -116,6 +116,32 @@ class Dipper
 	}
 
 
+    /**
+     * Takes a given file name and parses its content for YAML
+     *
+     * @param string  $file  The YAML file to parse
+     * @return array
+     * @throw \Exception
+     */
+    public static function parseFile($file)
+    {
+        if (!file_exists($file)) {
+            throw new \Exception('Missing file \''.$file.'\'!', 1);
+        }
+
+        $php = self::parse(file_get_contents($file));
+
+        if (empty($php)) {
+            throw new \Exception('\''.$file.'\' is empty!', 2);
+        }
+
+        if (!is_array($php)) {
+            throw new \Exception('Invalid file \''.$conf_file.'\'!', 3);
+        }
+
+        return $php;
+    }
+
 	/**
 	 * Makes YAML from PHP
 	 *
